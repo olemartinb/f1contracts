1745ef6478b6f9427d6dc7cd679295ac46d94ab7
Ole-Martin Bratteberg-Øye <ole-martin.bratteberg@twoday.com>
2025-02-14 13:44:19 +0100

    chore: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react";
 import App from './App.tsx';
 import './index.css';
 
@@ -11,5 +12,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
